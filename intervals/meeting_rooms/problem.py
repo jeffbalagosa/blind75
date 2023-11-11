d50da9bf94295e3d9d7c8fe8807785fbb464aeab
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-11-10 21:46:36 -0800

    Reset problem.

--- intervals/meeting_rooms/problem.py
+++ intervals/meeting_rooms/problem.py
@@ -14,9 +14,4 @@ class Solution:
     """
 
     def can_attend_meetings(self, intervals: List[List[int]]) -> bool:
-        intervals = [Interval(start, end) for start, end in intervals]
-        intervals.sort(key=lambda x: x.start)
-        for i in range(1, len(intervals)):
-            if intervals[i].start < intervals[i - 1].end:
-                return False
-        return True
+        pass
