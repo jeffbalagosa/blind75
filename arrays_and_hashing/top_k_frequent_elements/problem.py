05bec8b1e110828a66562b75299b0ecbc22dadb9
JeffBalagosa <jeff.balagosa@vegas.com>
2024-12-14 08:35:24 -0800

    Reset problem.

--- arrays_and_hashing/top_k_frequent_elements/problem.py
+++ arrays_and_hashing/top_k_frequent_elements/problem.py
@@ -5,16 +5,3 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-
-        arr = []
-        for num, cnt in count.items():
-            arr.append([cnt, num])
-        arr.sort()
-
-        res = []
-        while len(res) < k:
-            res.append(arr.pop()[1])
-        return res
