f5cb2ced9d3982247f55264f1b7192714b7d29b5
JeffBalagosa <jeff.balagosa@vegas.com>
2024-12-14 15:09:13 -0800

    Reset problem.

--- arrays_and_hashing/product_of_array_except_self/problem.py
+++ arrays_and_hashing/product_of_array_except_self/problem.py
@@ -4,15 +4,3 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        res = [0] * n
-
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i == j:
-                    continue
-                prod *= nums[j]
-
-            res[i] = prod
-        return res
